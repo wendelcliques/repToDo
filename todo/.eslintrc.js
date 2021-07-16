ee7fa27964923192ef93b05dca24ce1364f3350e
wendelcliques <70993835+wendelcliques@users.noreply.github.com>
2021-07-16 01:38:08 -0300

    atual

--- todo/.eslintrc.js
+++ todo/.eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
     env: {
       node: true,
       commonjs: true,
@@ -33,4 +33,4 @@ module.exports = {
       ],
     },
   }
-  
+  */
--- todo/src/App.js
+++ todo/src/App.js
@@ -1,19 +1,61 @@
 import React, {useState} from 'react';
 
+import { MdDelete } from 'react-icons/md';
+
 import './App.css';
 
 const App = () => {
+    const ESCAPE_KEY = 27;
+    const ENTER_KEY = 13;
+
+
+
+const [todos, setTodos] = useState([]);
 const [value, setValue] = useState('');
 
-const onChange = (event) => {
+const erase = () => {
+    setValue('');
+}
 
+const submit = () => {
+ 
+
+    setTodos([
+        ...todos,
+        {
+             id: new Date().getTime() ,
+             title: value,
+             checked: false
+        }
+    ]);
+    erase();
+}
+
+const onChange = (event) => {
     setValue(event.target.value);
 }
 
 const onKeyDown = (event) => {
-if (event.which == 13) {
-    console.log(value);
+if (event.which == ENTER_KEY) {
+   submit();
+} else if (event.which == ESCAPE_KEY) {
+    erase();
 }
+}
+
+const onToggle = (todo) => {
+setTodos(todos.map((obj) => 
+(obj.id === todo.id ? {...obj, checked: !todo.checked} : obj
+)));
+
+
+
+console.log('toggle', todos);
+}
+
+const onRemove = (todo) => {
+    console.log('remove', todo);
+    setTodos(todos.filter((obj) => obj.id !== todo.id))
 }
 
 return(
@@ -22,13 +64,34 @@ return(
         <h1 className="title">todo</h1>
         </header>
     <section className="main">
-    <input
-    className="new-todo" 
-    placeholder="o que precisa ser feito?"
-    value={value}
-    onChange={onChange}
-    onKeyDown={onKeyDown}
-    />
+         <input
+            className="new-todo" 
+            placeholder="o que precisa ser feito?"
+            value={value}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+        />
+
+        <ul className="todo-list">
+            {todos.map((todo) => (
+                    <li key={todo.id.toString()} >
+                        <span 
+                        className={["todo", todo.checked ? "checked" : ''].join(' ')}
+                        onClick={() => onToggle(todo)}
+                        onKeyPress={() => onToggle(todo)}
+                        role="button"
+                        tabIndex={0}
+                        >{todo.title}</span>
+                        <button 
+                        className="remove" 
+                        type="button"
+                        onClick={() => onRemove(todo)}
+                        >
+                            <MdDelete size={28} />
+                        </button>
+                    </li>
+            ))}
+        </ul>
     </section>
     
     </section>
